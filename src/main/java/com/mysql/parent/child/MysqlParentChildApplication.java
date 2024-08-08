f34b70e2fa20a9909f5c5babc74459e01060906e
unknown <rahul.bhawar.threeplus@gmail.com>
2024-08-08 18:01:43 +0530

    Initial commit

---
+++ src/main/java/com/mysql/parent/child/MysqlParentChildApplication.java
@@ -0,0 +1,13 @@
+package com.mysql.parent.child;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlParentChildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlParentChildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysql/parent/child/MysqlParentChildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysql.parent.child;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlParentChildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
